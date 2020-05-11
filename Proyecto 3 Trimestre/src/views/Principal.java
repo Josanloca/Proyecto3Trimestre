35d66c59599fd04511f443f1f16eeb1b7fc83ad4
josanloca <josanloca@DESKTOP-1D866LG>
2020-05-11 15:12:09 +0200

    Actualizacion nº 6 (Empezando database y modelos)

--- Proyecto 3 Trimestre/src/views/Principal.java
+++ Proyecto 3 Trimestre/src/views/Principal.java
@@ -1,9 +1,17 @@
 package views;
 
+import Controllador.ConexionBaseDatos;
+
 public class Principal {
 
 	public static void main(String[] args) {
 
+		
+		ConexionBaseDatos  d= new ConexionBaseDatos("tienda");
+		
+		if(ConexionBaseDatos.checkConnectionDatabase()) {
+			System.out.println("Retraso perfecto");
+		}
 		
 	}
 
